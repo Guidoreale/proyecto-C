4c8a29ad60d9bb2985234d51f02e2b0f52215718
unknown <Tobias@DESKTOP-VEHNHJG>
2022-09-11 16:11:59 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+TColaCP crear cola cp(int (*f)(TEntrada, TEntrada)){
+    TColaCP cola = NULL;
+
+    return cola;
+}
+private int f(TEntrada, TEntrada){
+
+}
+
+typedef struct cola_con_prioridad {
+unsigned int cantidad_elementos;
+TNodo raiz;
+} * TColaCP;
+
+typedef struct nodo {
+TEntrada entrada;
+struct nodo * padre;
+struct nodo * hijo_izquierdo;
+struct nodo * hijo_derecho;
+} * TNodo;
+
+typedef struct entrada {
+TClave clave;
+TValor valor;
+} * TEntrada;
+
+typedef void * TClave;
+typedef void * TValor;
+
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
