2c5d4f2a5f09e944afcde8bef562bd8c9de8692c
Guido <Guido@DESKTOP-2QGGVKP>
2022-09-28 13:19:54 -0300

    agregación de crearColaCP.

--- main.c
+++ main.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-TColaCP crear cola cp(int (*f)(TEntrada, TEntrada)){
-    TColaCP cola = NULL;
-
-    return cola;
-}
-private int f(TEntrada arg1, TEntrada arg2){
-    if(arg1.clave>)
-    return -1;
-}
-
-typedef struct cola_con_prioridad {
-unsigned int cantidad_elementos;
-TNodo raiz;
-} * TColaCP;
-
-typedef struct nodo {
-TEntrada entrada;
-struct nodo * padre;
-struct nodo * hijo_izquierdo;
-struct nodo * hijo_derecho;
-} * TNodo;
-
-typedef struct entrada {
-TClave clave;
-TValor valor;
-} * TEntrada;
-
 typedef void * TClave;
 typedef void * TValor;
 
+typedef struct entrada {
+    TClave clave;
+    TValor valor;
+} * TEntrada;
 
+typedef struct nodo {
+    TEntrada entrada;
+    struct nodo * padre;
+    struct nodo * hijo_izquierdo;
+    struct nodo * hijo_derecho;
+} * TNodo;
+
+typedef struct cola_con_prioridad {
+    unsigned int cantidad_elementos;
+    TNodo raiz;
+    int (*comparador)(TEntrada, TEntrada);
+} * TColaCP;
+
+TColaCP crearCola(int (*f)(TEntrada, TEntrada)){
+    TColaCP nuevaCola = (TColaCP) malloc(sizeof(struct cola_con_prioridad));
+    nuevaCola ->cantidad_elementos = 0;
+    nuevaCola ->raiz = NULL;
+    nuevaCola ->comparador = (*f);
+    return nuevaCola;
+}
+
+ int f(TEntrada arg1, TEntrada arg2){
+    return -1;
+}
 
 
 int main()
