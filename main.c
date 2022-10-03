b4a912fe489e31ab294334071b3a5d67d7964099
Windows <Windows@Tobias>
2022-10-02 21:51:08 -0300

    cambios a preguntar

--- main.c
+++ main.c
@@ -22,6 +22,9 @@ typedef struct cola_con_prioridad {
     int (*comparador)(TEntrada, TEntrada);
 } * TColaCP;
 
+
+
+
 TColaCP crearCola(int (*f)(TEntrada, TEntrada)){
     TColaCP nuevaCola = (TColaCP) malloc(sizeof(struct cola_con_prioridad));
     nuevaCola ->cantidad_elementos = 0;
@@ -42,11 +45,104 @@ int f(TEntrada a, TEntrada b){
     return valor;
     }
 
+    int cp_cantidad(TColaCP cola){
+        return cola->cantidad_elementos;
+    } 
+
+   int logCasero(int numero){
+      int ret;
+      ret = (int)log(numero);
+      return ret;
+   }
+
+    int insertarPerfecto(TEntrada aInsertar,TColaCP cola){
+        TNodo *temp;
+        temp = &cola->raiz;
+        while((*temp)->hijo_izquierdo != NULL){
+            temp = (*temp)->hijo_izquierdo;
+        }
+        TNodo nuevo = (TNodo) malloc(sizeof(struct nodo));
+        nuevo->entrada = aInsertar;
+        (*temp)->hijo_izquierdo = nuevo;
+        nuevo->padre = temp;
+        cola->cantidad_elementos++;
+        //¿es correcto el usp del puntero, tendriamos que usar malloc y free?
+    }
+
+    int buscar(int contador, TEntrada aInsertar, TNodo nodo , TColaCP cola){
+        int cont = contador; // empieza en 1, la vuelta del derecho entra con cont = 2
+        int retorno = 0;
+        if (cont < (logCasero(cola->cantidad_elementos)/logCasero(2)) -1){
+            cont++;
+            retorno = buscar(cont,aInsertar,nodo->hijo_izquierdo,cola);
+            if (!retorno)
+                buscar(cont,aInsertar, nodo->hijo_derecho, cola);
+        }
+        else
+            if (nodo->hijo_izquierdo == NULL){
+                TNodo nuevo = (TNodo) malloc(sizeof(struct nodo));
+                nuevo->entrada = aInsertar;
+                nodo->hijo_izquierdo = nuevo;
+                retorno = 1;
+            }
+            else
+                if (nodo->hijo_derecho == NULL){
+                    TNodo nuevo = (TNodo) malloc(sizeof(struct nodo));
+                    nuevo->entrada = aInsertar;
+                    nodo->hijo_derecho = nuevo;
+                    retorno = 1;
+                }       
+    return retorno;
+}
+
+int insertar(TColaCP cola, TEntrada aInsertar){
+    int aRetornar = 0;
+    if (cola-> cantidad_elementos == 0){
+        TNodo nuevo = (TNodo) malloc(sizeof(struct nodo));
+        nuevo->entrada = aInsertar;
+        cola ->raiz = nuevo;
+        nuevo->padre = NULL;
+        cola ->cantidad_elementos++;
+        aRetornar = 1;
+    }
+    else
+        if (cola->cantidad_elementos == 2^((logCasero(cola->cantidad_elementos)/logCasero(2)+1))-1)
+            {
+                insertarPerfecto(aInsertar,cola);
+            }
+        else if ((logCasero(cola->cantidad_elementos)/logCasero(2)) == 0)
+            if (cola->raiz->hijo_izquierdo == NULL){
+                TNodo nuevo = (TNodo) malloc(sizeof(struct nodo));
+                nuevo->entrada = aInsertar;
+                cola ->raiz->hijo_izquierdo = nuevo;
+                nuevo->padre = cola->raiz;
+                aRetornar = 1;
+            }
+            
+            
+        else
+            aRetornar = buscar(1,aInsertar, cola->raiz,cola);  
+
+    return aRetornar;
+}
+
+
 
 
 
 int main()
 {
-    printf("Hello world!\n");
+    TColaCP cola = crearCola(f);    
+    TEntrada entr;
+    entr->clave = (TClave) 1;
+    entr->valor = (TValor) 3;
+    insertar(cola, entr);
+
+
+
+   for(int i=0; i<4; i++){
+        printf("cola %i: \n", cola->raiz);
+        }
+
     return 0;
 }
