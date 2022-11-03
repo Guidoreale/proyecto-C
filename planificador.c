350cdb54e17b0a7b4ef5d53dfca151b2f87afc0d
Tobias <Windows@Tobias>
2022-11-03 00:55:52 -0300

    funcionando 100%

--- planificador.c
+++ planificador.c
@@ -7,13 +7,9 @@
 #include <windows.h>
 #endif
 
-
-
 float calcularDistancia(TCiudad ciudad, TUsuario us){
     return fabsf(ciudad->pos_x - us->pos_x) + fabsf(ciudad->pos_y - us->pos_y);
 }
-
-
 
 void mostrarAscendente(TCiudad* arr, int cont, TUsuario us){
         TColaCP nueva = crear_cola_cp(ordenAscendente);
@@ -28,10 +24,13 @@ void mostrarAscendente(TCiudad* arr, int cont, TUsuario us){
             cp_insertar(nueva, aCrear);
         }
         cont = 0;
-        //TODO cambiar por cp_cantidad
-        while(nueva->cantidad_elementos  > 0){
+
+        while(cp_cantidad(nueva)  > 0){
             TEntrada aMostrar = cp_eliminar(nueva);
             printf("Nº%d: %s \n",cont, (char *) (aMostrar->valor));
+            free(aMostrar->valor);
+            free(aMostrar->clave);
+            free(aMostrar);
             cont++;
         }
         cp_destruir(nueva, fEliminar);
@@ -50,24 +49,26 @@ void mostrarAscendente(TCiudad* arr, int cont, TUsuario us){
             cp_insertar(nueva, aCrear);
         }
         cont = 0;
-        while(nueva->cantidad_elementos > 0){
+        while(cp_cantidad(nueva) > 0){
             TEntrada aMostrar = cp_eliminar(nueva);
             printf("Nº%d: %s \n",cont, (char*) aMostrar->valor);
+            free(aMostrar->valor);
+            free(aMostrar->clave);
+            free(aMostrar);
             cont++;
         }
         cp_destruir(nueva, fEliminar);
     }
 
-
-
-    void reducirHorasDeManejo(TCiudad* arr, int cont, TUsuario us){
+    void reducirHorasDeManejo(TCiudad* arr, int size, TUsuario us){
         float* prioCiudad;
         TColaCP nueva = crear_cola_cp(ordenAscendente);
         TColaCP secundaria = crear_cola_cp(ordenAscendente);
         TCiudad c;
+        float distanciaRecorrida=0;
         //chequear los malloc
         //cont-1 tiene que ser size
-        for (int i = 0; i < cont - 1; i++) {
+        for (int i = 0; i < size; i++) {
             TEntrada aCrear = (TEntrada) malloc(sizeof(struct entrada));
             prioCiudad = (float *) malloc(sizeof(float));
             *prioCiudad = calcularDistancia(arr[i], us);
@@ -75,27 +76,35 @@ void mostrarAscendente(TCiudad* arr, int cont, TUsuario us){
             aCrear->valor = arr[i];
             cp_insertar(nueva, aCrear);
         }
-        int restantes = cont -1;
+        int restantes = size;
 
         while (restantes > 0) {
             TEntrada temporal;
             temporal = cp_eliminar(nueva);
+
             restantes--;
             c = (TCiudad) temporal->valor;
+
             us->pos_x = c->pos_x;
             us->pos_y = c->pos_y;
-            printf("ciudad Numero %d: %s \n", cont - 1 - restantes, (char*) ((TCiudad)temporal->valor)->nombre);
+            distanciaRecorrida = distanciaRecorrida + *((float*) (temporal->clave));
+            printf("ciudad Numero %d: %s \n", size - restantes, (char*) ((TCiudad)temporal->valor)->nombre);
+            free(temporal->clave);
+            free(temporal);
 
-            while (nueva->cantidad_elementos != 0){
+            while (cp_cantidad(nueva)  != 0){
                 temporal = cp_eliminar(nueva);
                 *(float*)temporal->clave = calcularDistancia(temporal->valor, us);
                 cp_insertar(secundaria, temporal);
+
             }
 
             TColaCP aux = secundaria;
             secundaria = nueva;
             nueva = aux;
+
         }
+        printf("Total recorrido: %.1f\n",distanciaRecorrida);
         cp_destruir(nueva,fEliminar);
         cp_destruir(secundaria, fEliminar);
     }
@@ -136,34 +145,47 @@ int main(int argc, char* argv[]){
         arr[i]->nombre = strCiudad;
         fscanf(archivo,"%[^;];%f;%f\n",arr[i]->nombre, &arr[i]->pos_x, &arr[i]->pos_y);
     }
+    int size;
+    size = cont-1;
     fclose(archivo);
 
-    printf("Ingrese una accion a realizar:\n");
-    printf("1: Mostrar ascendente\n");
-    printf("2: Mostrar descendente\n");
-    printf("3: Reducir horas de manejo\n");
-    printf("4: Salir\n");
-    int leerNumero;
-    scanf("%i",&leerNumero);
-    switch (leerNumero) {
-        case 1:
-            mostrarAscendente(arr, cont - 1, usuario1);
-            break;
-        case 2:
-            mostrarDescendente(arr, cont - 1, usuario1);
-            break;
-        case 3:
-            reducirHorasDeManejo(arr, cont, usuario1);
-            break;
-        case 4:
-            //Limpiar ciudades
-            free(usuario1);
-            free(arr);
-            break;
 
-        default:
-            printf("Fuera de rango");
-            break;
+    int leerNumero = 0;
+    while(leerNumero!=4){
+        printf("Ingrese una accion a realizar:\n");
+        printf("1: Mostrar ascendente\n");
+        printf("2: Mostrar descendente\n");
+        printf("3: Reducir horas de manejo\n");
+        printf("4: Salir\n");
+
+        scanf("%i",&leerNumero);
+        if(leerNumero != 4)
+        switch (leerNumero) {
+            case 1:
+                    mostrarAscendente(arr, size, usuario1);
+                    printf(" \n");
+                break;
+            case 2:
+                    mostrarDescendente(arr, size, usuario1);
+                    printf(" \n");
+                break;
+            case 3:
+                    reducirHorasDeManejo(arr, size, usuario1);
+                    printf(" \n");
+                break;
+            case 4:
+                for(int i = 0;i<size;i++){
+                    free(arr[i]->nombre);
+                    free(arr[i]);
+                }
+                free(usuario1);
+                free(arr);
+                break;
+
+            default:
+                printf("Fuera de rango\n");
+                break;
+        }
     }
 
 
